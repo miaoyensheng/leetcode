90c5c81b2e4492c917bb98847aff59f321c332ba
miaoyensheng <miaoyensheng@gmail.com>
2019-09-23 21:30:06 +1000

    update number-factor algorithm

--- number-factor/NumberFactor.py
+++ number-factor/NumberFactor.py
@@ -3,7 +3,13 @@
 def NumberFactor(n):
     if n == 0 or n == 1 or n == 2 :
         return 1
-    elif n == 3 :
-        return 2
+    elif n < 0 :
+        return 0
     else:
-        NumberFactor
+        num1 = NumberFactor(n-1)
+        num2 = NumberFactor(n-3)
+        num3 = NumberFactor(n-4)
+
+        return num1+num2+num3
+
+print(NumberFactor(5))
